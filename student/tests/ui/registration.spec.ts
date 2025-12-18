6f7fb366ce9b33b5f8b454d202af1ef317ee3405
nzainuddin <nadzainuddin@gmail.com>
2025-12-19 00:02:46 +0800

    add missing registration test page

--- student/tests/ui/registration.spec.ts
+++ student/tests/ui/registration.spec.ts
@@ -1,25 +1,25 @@
-import { test } from '../../../fixtures/fixtures';
+import { test } from '../../../fixtures/studentFixtures';
 
-test('Submit registration form having only mandatory details', async ({ registrationPage }) => {
-  await registrationPage.visit();
-  await registrationPage.fillMandatoryFields();
-  await registrationPage.submitBtn.click();
+test('Submit registration form having only mandatory details', async ({ pages }) => {
+  await pages.registrationPage.visit();
+  await pages.registrationPage.fillMandatoryFields();
+  await pages.registrationPage.submitBtn.click();
 });
 
-test('Submit registration form with full details', async ({ registrationPage }) => {
-  await registrationPage.visit();
-  await registrationPage.fillAllFields();
-  await registrationPage.submitBtn.click();
+test('Submit registration form with full details', async ({ pages }) => {
+  await pages.registrationPage.visit();
+  await pages.registrationPage.fillAllFields();
+  await pages.registrationPage.submitBtn.click();
 });
 
-test('Submit registration with phone number less than 10 digits', async ({ registrationPage }) => {
-  await registrationPage.visit();
-  await registrationPage.fillMandatoryFields(); 
-  await registrationPage.submitBtn.click();
+test('Submit registration with phone number less than 10 digits', async ({ pages }) => {
+  await pages.registrationPage.visit();
+  await pages.registrationPage.fillMandatoryFields(); 
+  await pages.registrationPage.submitBtn.click();
 });
 
-test('Submit registration without completing mandatory fields', async ({ registrationPage }) => {
-  await registrationPage.visit();
-  await registrationPage.fillAllFields(); 
-  await registrationPage.submitBtn.click();
+test('Submit registration without completing mandatory fields', async ({ pages }) => {
+  await pages.registrationPage.visit();
+  await pages.registrationPage.fillAllFields(); 
+  await pages.registrationPage.submitBtn.click();
 });
